d04e909803a7d1c046ae8421419ef404e8c2c095
Bill Chou (Zen3 Infosolutions America Inc) <v-tincho@microsoft.com>
2020-04-28 17:25:07 -0700

    Create popup dialog and its styles
    index.ts
    1. Create popup dialog
    2. Use "getElementsByClassName(`${styles.className}`)" to select
       element, since we want to prevent using id
    3. Use "addEventListener(click)" in "More info" button to show the
       popup dialog
    4. Use "addEventListener(click)" in "x" to hide the popup dialog
    5. Uncomment "addEventListener(touchstart)" in case browsers do
       not transform Mouse Events to Touch Events.
    https://stackoverflow.com/questions/28987034/button-not-working-on-mobile-devices-but-works-on-pc-bootstrap
    https://stackoverflow.com/questions/42123283/chrome-56-mobile-addeventlistener-click-not-working
    
    styles.scss
    1. Add prefix to support older browser, if "display: flex"
    2. Remove redundant styles
    3. Add styles for popup dialog
    4. Adjust the width and height properties in some elements to
       make the view consistent with the poster
    5. Since we want to prevent using id, wrap the radio button
       into label and use "display: inline-table" and "display:
       table-cell"
    6. Use "position: relative" and "top" to adjust the
       relative position between radio button and label
    7. Add prefix to support older browser and default
       value in case "calc()" is not supported

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,133 @@ const banner =
             <button type="button" class="${styles.bannerButton}">More info</button>
         </div>
     </div>
+
+    <!-- The Modal -->
+    <div class="${styles.cookieModal}">
+        <div role="dialog" aria-modal="true" aria-label="Flow scroll" class="${styles.modalContainer}">
+            <span role="button" aria-label="Close dialog" class="${styles.closeModalIcon}">&times;</span>
+            <div role="setting" class="${styles.modalBody}">
+                <div class="${styles.modalTitle}">Manage cookie preferences</div>
+                
+                <div class="${styles.modalContent}">
+                    <p class="${styles.cookieStatement}">
+                        Most Microsoft sites use cookies, small text files placed on your device which web servers in 
+                        the domain that placed the cookie can retrieve later. We use cookies to store your preferences 
+                        and settings, help with sign-in, provide targeted ads, and analyze site operations. 
+                        For more information, see the 
+                        <a href="https://privacy.microsoft.com/en-us/privacystatement#maincookiessimilartechnologiesmodule" target="_blank">
+                            Cookies and similar technologies section of the Privacy Statement
+                        </a>.
+                    </p>
+
+                    <ol class="${styles.cookieOrderedList}">
+                        <li class="${styles.cookieListItem}">Essential cookies</li>
+                        <span class="${styles.cookieListItemDescription}">
+                            We use essential cookies to do things.
+                        </span>
+                
+                        <li class="${styles.cookieListItem}">Performance & analytics</li>
+                        <span class="${styles.cookieListItemDescription}">
+                            We use performance & analytics cookies to track how things are working. Message text. This 
+                            is where the message dialog text goes. The text can wrap and wrap and wrap and wrap.
+                        </span>
+                        <div class="${styles.cookieItemRadioBtnGroup}">
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="performanceCookies" class="${styles.cookieItemRadioBtn}" value="accept">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Accept</span>
+                            </label>
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="performanceCookies" class="${styles.cookieItemRadioBtn}" value="reject">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Reject</span>
+                            </label>
+                        </div>
+
+                        <li class="${styles.cookieListItem}">Advertising/Marketing</li>
+                        <span class="${styles.cookieListItemDescription}">
+                            We use advertising/marketing cookies to provide our partners with data. Message text. This 
+                            is where the message dialog text goes. The text can wrap and wrap and wrap and wrap.
+                        </span>
+                        <div class="${styles.cookieItemRadioBtnGroup}">
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="advertisingCookies" class="${styles.cookieItemRadioBtn}" value="accept">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Accept</span>
+                            </label>
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="advertisingCookies" class="${styles.cookieItemRadioBtn}" value="reject">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Reject</span>
+                            </label>
+                        </div>
+
+                        <li class="${styles.cookieListItem}">Targeting/personalization</li>
+                        <span class="${styles.cookieListItemDescription}">
+                            We use targeting/personalization cookies to enhance the quality of ads you see. Message text. 
+                            This is where the message dialog text goes. The text can wrap and wrap and wrap and wrap.
+                        </span>
+                        <div class="${styles.cookieItemRadioBtnGroup}">
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="targetingCookies" class="${styles.cookieItemRadioBtn}" value="accept">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Accept</span>
+                            </label>
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="targetingCookies" class="${styles.cookieItemRadioBtn}" value="reject">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Reject</span>
+                            </label>
+                        </div>
+
+                        <li class="${styles.cookieListItem}">Social media</li>
+                        <span class="${styles.cookieListItemDescription}">
+                            We use social media cookies to improve the experience you see. Message text. This is where 
+                            the message dialog text goes. The text can wrap and wrap and wrap and wrap.
+                        </span>
+                        <div class="${styles.cookieItemRadioBtnGroup}">
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="socialMediaCookies" class="${styles.cookieItemRadioBtn}" value="accept">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Accept</span>
+                            </label>
+                            <label class="${styles.cookieItemRadioBtnCtrl}">
+                                <input type="radio" name="socialMediaCookies" class="${styles.cookieItemRadioBtn}" value="reject">
+                                <span class="${styles.cookieItemRadioBtnLabel}">Reject</span>
+                            </label>
+                        </div>
+                    </ol>
+                </div>
+                
+                <div class="${styles.modalButtonGroup}">
+                    <button type="button" class="${styles.modalButtonSave}">Save changes</button>
+                    <button type="button" class="${styles.modalButtonReset}">Reset all</button>
+                </div>
+            </div>
+        </div>
+    </div>
 `;
 
 if (insert) {
     insert.innerHTML = banner;
+}
+
+let cookieInfo = document.getElementsByClassName(`${styles.bannerButton}`)[2];
+let modal: HTMLElement = <HTMLElement> document.getElementsByClassName(`${styles.cookieModal}`)[0];
+let closeModalIcon = document.getElementsByClassName(`${styles.closeModalIcon}`)[0];
+
+function popup() {
+    if (modal) {
+        modal.style.display = 'block';
+    }
+}
+
+function close() {
+    if (modal) {
+        modal.style.display = 'none';
+    }
+}
+
+if (cookieInfo) {
+    cookieInfo.addEventListener('click', popup);
+
+    // Add this line in case some browsers in mobile do not like click event 
+    // cookieInfo.addEventListener('touchstart', popup);
+}
+
+if (closeModalIcon) {
+    closeModalIcon.addEventListener('click', close);
 }
